1e37686c4d2a58ff496b25fb78bc579fe296df3b
Brunno Nogueira <brunno.nogueira4j@gmail.com>
2022-04-30 18:05:28 -0300

    comment UseCase files

--- application/src/main/java/com/nogueira/catalog/admin/application/UseCase.java
+++ application/src/main/java/com/nogueira/catalog/admin/application/UseCase.java
@@ -3,7 +3,7 @@ package com.nogueira.catalog.admin.application;
 import com.nogueira.catalog.admin.domain.category.Category;
 
 public class UseCase {
-    public Category execute() {
-        return Category.newCategory();
-    }
+//    public Category execute() {
+//        return Category.newCategory();
+//    }
 }
